5b2e4e716b401b63c5be8f61bccdfe6d0be09e56
Rishang <rishangbhavsarcs@gmail.com>
2022-03-20 13:10:45 +0530

    main branch env

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 if __name__ == "__main__":
     env = os.environ.get("FLASK_ENV")
-    if isinstance(env, str) and "master" in env.lower():
+    if isinstance(env, str) and "main" in env.lower():
         app.run()
     else:
         app.run(debug=True)
