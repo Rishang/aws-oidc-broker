7d8b2e4fb3281b9118c140935e2faf49868185e0
Random._ <rishangbhavsarcs@gmail.com>
2023-01-17 22:10:04 +0530

    feat: device auth

--- Dockerfile
+++ Dockerfile
@@ -5,22 +5,29 @@ EXPOSE 5000
 
 # Keeps Python from generating .pyc files in the container
 ENV PYTHONDONTWRITEBYTECODE=1
-
 # Turns off buffering for easier container logging
 ENV PYTHONUNBUFFERED=1
+
+RUN pip install -U pip setuptools; pip install -U poetry
 
 WORKDIR /app
 
 RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
 
-# Install pip requirements
-COPY requirements.txt .
-RUN python -m pip install --no-cache-dir -r requirements.txt
+# copy pre-required files for dependency installation
+COPY scripts ./scripts
+COPY pyproject.toml ./
+COPY poetry.lock ./
+COPY poetry.toml ./
+RUN python ./scripts/_docker_poetry_fix.py
 
+# # Creates a non-root user with an explicit UID and adds permission to access the /app folder
 USER appuser
-COPY broker /app
-# Creates a non-root user with an explicit UID and adds permission to access the /app folder
-# For more info, please refer to https://aka.ms/vscode-docker-python-configure-containers
+# Install pip requirements
+RUN poetry install --no-dev --no-cache --without=docs
 
-# During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
-CMD ["gunicorn", "--bind", "0.0.0.0:5000", "routes:app"]
+COPY broker ./
+# # For more info, please refer to https://aka.ms/vscode-docker-python-configure-containers
+
+# # During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
+CMD ["bash", "-c", "source .venv/bin/activate; gunicorn --bind 0.0.0.0:5000 routes:app"]
---
+++ device/config.py
@@ -0,0 +1,82 @@
+import configparser
+from dataclasses import dataclass
+from typing import Optional
+import re
+
+import requests
+import typer
+
+from utils import State, HOME, Logger
+
+STATE_CONFIG_FILE = "../tmp/test.json"
+
+logger = Logger()
+
+
+@dataclass
+class Profile:
+    role_arn: str
+    client_wellknown: str
+    client_id: str
+    audience: Optional[str] = ""
+
+
+@dataclass
+class AwsConfig:
+    """ref: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html"""
+
+    web_identity_token_file: str
+    role_arn: str
+
+
+profiles = State(STATE_CONFIG_FILE, obj=Profile)
+
+
+def awsconfig(
+    profile: str, aws_config: AwsConfig, awsconfig_file=f"{HOME}/.aws/config"
+):
+    profile = f"profile {profile}"
+
+    config = configparser.ConfigParser()
+    config.read(awsconfig_file)
+    write_flag = False
+
+    if not config.has_section(profile):
+        config.add_section(profile)
+
+    for arg in aws_config.__annotations__:
+        if not config.has_option(profile, arg):
+            config.set(profile, arg, aws_config.__dict__[arg])
+            write_flag = True
+
+        elif aws_config.__dict__[arg] != config[profile][arg]:
+            config[profile][arg] = aws_config.__dict__[arg]
+            write_flag = True
+
+    if write_flag:
+        with open(awsconfig_file, "w") as configfile:
+            config.write(configfile)
+
+
+def _check_aws_iam(arn: str):
+    if re.search(r"^(arn:aws:iam::)([0-9]{12}):role\/([a-zA-Z0-9\-]+)", arn):
+        return arn
+    else:
+        raise typer.BadParameter("Invalid iam role arn.")
+
+
+def _check_wellknown_openid(url: str):
+
+    domain = re.sub(r"(https?:\/\/)?(\.well-known.+)?", "", url)
+
+    if domain[-1] == "/":
+        domain = domain[:-1]
+
+    try:
+        requests.get(f"https://{domain}/.well-known/openid-configuration").json()
+    except:
+        raise typer.BadParameter(
+            "Invalid openid-configuration domain.", param_hint="op"
+        )
+
+    return domain
